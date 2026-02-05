3343cf3da36258438350e2049805b97a4845b723
BrandonLee <15escamillabrandon@gmail.com>
2026-02-05 11:10:18 -0600

    first commit

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    //asi tambien funciona y se realiza cuando se necesitan algunas comprobaciones
+    //const category = new Category();
+    //category.name = createCategoryDto.name;
+    return this.categoryRepository.save(createCategoryDto);
+  }
+
+  findAll() {
+    return this.categoryRepository.find();
+  }
+
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOneBy({ id });
+    if (!category) {
+      throw new NotFoundException('La Categoria no existe');
+    }
+    return category;
+  }
+
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOne(id);
+    category.name = updateCategoryDto.name;
+    return await this.categoryRepository.save(category);
+  }
+
+  async remove(id: number) {
+    const category = await this.findOne(id);
+    await this.categoryRepository.remove(category);
+    return 'categoria eliminada';
+  }
+}
---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,15 @@
+//aqui es todo el mapeo o estructura de la base de datos
+import { Product } from 'src/products/entities/product.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+  //esta parte es para relacionar con otro base de datos en su caso la de productos
+  @OneToMany(() => Product, (product) => product.category, { cascade: true })
+  products: Product[];
+}
